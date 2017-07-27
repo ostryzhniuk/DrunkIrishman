8f3d276d357b82cea6208088b46dc2b12d7600b0
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-07-27 21:22:45 +0300

    createCategory button created

--- src/main/webapp/catalog/categories/catalog-categories.component.js
+++ src/main/webapp/catalog/categories/catalog-categories.component.js
@@ -8,14 +8,35 @@ angular.
 module('catalogCategories').
 component('catalogCategories', {
     templateUrl: '/catalog/categories/catalog-categories.template.html',
-    controller: ['$http', '$scope', '$routeParams',
-        function CatalogCategoriesController($http, $scope, $routeParams) {
+    controller: ['$http', '$scope', '$routeParams', '$rootScope',
+        function CatalogCategoriesController($http, $scope, $rootScope) {
 
             $http.get('/categories').then(function(response) {
                 $scope.categories = response.data;
             });
 
             $scope.orderProp = 'name';
+
+            $http.get('/currentUser').then(function(response) {
+                $rootScope.user = response.data;
+            });
+
+            $scope.isAuthority = function (role) {
+                if ($rootScope.user == undefined) {
+                    return false;
+                }
+                var authorities = $rootScope.user.authorities;
+                for (var authority in authorities) {
+                    if (authorities[authority].authority == role) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            $scope.createCategory = function() {
+
+            }
 
         }
     ]
