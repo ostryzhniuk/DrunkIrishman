91926a460092a15843490413e23dd81e914fd7fd
ostryzhniuk <andrii.ostryzhniuk@gmail.com>
2017-08-02 22:07:17 +0300

    OrderContent window styling

--- src/main/webapp/order/order.content/order-content.controller.js
+++ src/main/webapp/order/order.content/order-content.controller.js
@@ -11,11 +11,16 @@ module('orderContent')
         method: 'GET',
         url: '/order/content?orderId=' + order.id
     }).then(function(response) {
-        $scope.orderContent = response.data;
-        console.log($scope.orderContent);
+        $scope.orderContentList = response.data;
+        console.log($scope.orderContentList);
+
     });
 
     $scope.cancel = function () {
         $rootScope.modalInstance.close();
+    };
+
+    $scope.sumPrice = function (product, quantity){
+        return (quantity * product.price).toFixed(2);
     };
 });
